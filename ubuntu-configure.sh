2f06afe4d0d80333689eab5a97f7bc04889bbe36
David <davidryder@yahoo.com>
2020-05-12 16:53:04 -0700

    config os

--- ubuntu-configure.sh
+++ ubuntu-configure.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 
+# Install Chrome and Chromedriver
 
 sudo apt-get update
 curl -sS -o - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add
@@ -12,5 +13,11 @@ unzip -o chromedriver_linux64.zip
 sudo cp chromedriver /usr/local/bin/
 sudo chown root:root /usr/local/bin/chromedriver
 sudo chmod +x /usr/local/bin/chromedriver
-
 chromedriver --version
+
+
+# Install Python3 and Pip3
+sudo apt-get install python3 python3-pip
+
+# Install selenium
+pip3 install Selenium
