6059796ed8296eb1c031b332dce858c46ef81717
David <davidryder@yahoo.com>
2020-05-12 16:42:06 -0700

    unzip

--- install-chrome.sh
+++ install-chrome.sh
@@ -8,7 +8,7 @@ sudo add-apt-repository "deb [arch=amd64]  http://dl.google.com/linux/chrome/deb
 sudo apt-get -y update
 sudo apt-get -y install google-chrome-stable
 wget https://chromedriver.storage.googleapis.com/2.41/chromedriver_linux64.zip
-unzip chromedriver_linux64.zip
+unzip -f chromedriver_linux64.zip
 sudo cp chromedriver /usr/local/bin/
 sudo chown root:root /usr/local/bin/chromedriver
 sudo chmod +x /usr/local/bin/chromedriver
