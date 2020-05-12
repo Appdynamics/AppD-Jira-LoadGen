78bbd661acb67562ed8a761c7af0b5e19e8c7322
David <davidryder@yahoo.com>
2020-05-12 16:37:54 -0700

    Initial

---
+++ install-chrome.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+
+sudo apt-get update
+curl -sS -o - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add
+sudo add-apt-repository "deb [arch=amd64]  http://dl.google.com/linux/chrome/deb/ stable main" 
+sudo apt-get -y update
+sudo apt-get -y install google-chrome-stable
+wget https://chromedriver.storage.googleapis.com/2.41/chromedriver_linux64.zip
+unzip chromedriver_linux64.zip 
+sudo cp chromedriver /usr/local/bin/
+sudo chown root:root /usr/local/bin/chromedriver 
+sudo chmod +x /usr/local/bin/chromedriver 
+
+
